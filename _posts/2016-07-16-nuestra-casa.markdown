083a0fe8f729081cd1871dde6227c1d8e29949e3
bchartoff <bchartoff@gmail.com>
2016-07-16 14:37:37 -0500

    first post

--- _posts/2016-07-16-nuestra-casa.markdown
+++ _posts/2016-07-16-nuestra-casa.markdown
@@ -8,34 +8,32 @@ We're in Lima!!! This blog will be a place to share stories about our life and a
 
 For a first post, we want to give you a tour of our home. We live in Miraflores, a beautiful neighborhood in downtown Lima. When you walk in, you see our living room/dining room. How stylish is this?
 
-![](/assets/img/nuestra_casa/living_room.jpg)
+![](/assets/img/circle-cat.png)
 *great couch right?*
 
 We're also thrilled to finally have an apartment with an open kitchen! It's small but mighty. No oven, but we're going to see if we can make a combination of appliances work: a [fancy multi-use crock pot](http://www.cnet.com/products/ninja-cooking-system/), a grill, and a toaster oven. And it's open to the living room! Planning on many, many dinner parties here, and I won't be holed up in the kitchen. Hooray!
 
-![](/assets/img/nuestra_casa/kitchen.jpg)
+![](/assets/img/circle-cat.png)
 *This is where the magic happens*
 
 The living room also has this balcony! We're planning on planting some herbs in this planter. Shaping up to be a dream apartment.
 
-![](/assets/img/nuestra_casa/balcony.jpg)
+![](/assets/img/circle-cat.png)
 *Sometimes there are hummingbirds out here*
 
 Our bedroom is also lovely. Our landlord has pretty great taste.
 
-![](/assets/img/nuestra_casa/bedroom.jpg)
+![](/assets/img/circle-cat.png)
 *The nightstand with all the cool stuff on it is mine*
 
 And there's a second bedroom! This is where you'll be staying when you come visit. Stylish, roomy, a great home base for you Lima adventures. Come visit, seriously, look how great this bedroom is. It's also my office, and Natalie's clothes room.
 
-![](/assets/img/nuestra_casa/guest.jpg)
+![](/assets/img/circle-cat.png)
 *That bed's as comfortable as it looks. Come visit for serious.*
 
 And the bathroom is pretty cool too.
 
-![](/assets/img/nuestra_casa/bano.jpg)
+![](/assets/img/circle-cat.png)
 *Love those tiles on the wall*
-
-Lots more posts coming soon, can't wait to share our stories and memories with you!
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,27 +11,62 @@ var getRightGutter = function(){
 var leftGutter = getLeftGutter();
 var rightGutter = getRightGutter();
 
+d3.select("#bg1")
+	.style("left", function(){
+		console.log( (window.innerWidth - this.getBoundingClientRect().width)*.5)
+		return (window.innerWidth - this.getBoundingClientRect().width)*.5 + "px"
+	})
+d3.select("#bg2")
+	.style("left", function(){
+		console.log( (window.innerWidth - this.getBoundingClientRect().width)*.5)
+		return (window.innerWidth - this.getBoundingClientRect().width)*.5 + "px"
+	})
+
 d3.select(window)
 	.on("resize", function(){
 		leftGutter = getLeftGutter();
 		rightGutter = getRightGutter();
+
+		d3.selectAll(".page-content")
+			.style("margin-top", function(){
+				return window.innerWidth + "px";
+			})
+			.style("margin-bottom", function(){
+				return window.innerWidth/4	 + "px";
+			})
+
 	})
+	d3.selectAll(".page-content")
+		.style("margin-top", function(){
+			return window.innerWidth + "px";
+		})
+		.style("margin-bottom", function(){
+			return window.innerWidth/4 + "px";
+		})
+
+
+// $(document).on("load",function(){
+// 	if($(".post-list") == null){
+// 		window.scrollTo(0,window.innerWidth)
+// 	}
+// })
 
 d3.select(window)
 	.on("scroll",function(){
-		console.log("foo")
-
 		d3.select("#bg1")
 			.style("left",function(){
-				return Math.max(leftGutter, -1*window.scrollY) + "px"
+				return Math.max(leftGutter, (window.innerWidth - this.getBoundingClientRect().width)*.5-1*window.scrollY) + "px"
 			})
 		d3.select("#bg2")
 			.style("left",function(){
-				return Math.min(rightGutter, 1*window.scrollY) + "px"
+				return Math.min(rightGutter, (window.innerWidth - this.getBoundingClientRect().width)*.5+1*window.scrollY) + "px"
 			})
 
 		d3.select("body")
-			.transition()
-			.duration(2000)
-			.style("background","#fff")
+			// .transition()
+			// .duration(2000)
+			.style("background",function(){
+				var f = d3.interpolate("#20AF4B","#ffffff")
+				return f(window.scrollY/window.innerHeight*.5)
+			})
 	})
